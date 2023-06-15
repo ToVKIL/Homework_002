0c7412d84bc45fff4c2d2f83c11f7242f7a1d0a1
ToVKIL <yulia-belogorsk@mail.ru>
2023-06-15 20:39:07 +0900

    Addet Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num2 = num % 100;
+int num3 = num2 / 10;
+
+Console.Write(num + "->" + num3);
